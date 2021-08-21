1593d03d4b7fa6f9697eab38a1f6f45fb4616487
shindeanant <shindeanant1997@gmail.com>
2021-08-21 21:59:47 +0530

    eployee report

---
+++ pb5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+wageperhr=20
+fulldayhr=8
+monthdays=20
+rannum=$((RANDOM%2))
+
+        if [[ rannum -eq 1 ]]
+then
+        echo "Present"
+	dailywage=$(( $wageperhr * fulldayhr ))
+echo $dailywage
+monthwage=$(( $dailywage * $monthdays))
+echo $monthwage
+else
+        echo "Absent"
+fi
